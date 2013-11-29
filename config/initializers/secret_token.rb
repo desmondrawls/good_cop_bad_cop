bf68773d8478ef04876e1a77e45d1cc562442e5b
Desmond Rawls <captaingrover@gmail.com>
2013-11-28 21:24:08 -0500

    Initial commit. Adds gems and writes Readme.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoodCop::Application.config.secret_key_base = '36e17e6cd578b6e26cc4fd4b58f1a5780545b7c7a8c8029d9092f8937830383e20654d7ac9583a759a63c607e9e8b776dc24e20be1222212e4a06a7fdef77623'
