b8c61193f795dbdc80666f285416fcfcc2cfec64
Desmond Rawls <captaingrover@gmail.com>
2013-11-30 13:00:12 -0500

    Refactors cops_spec with a before loop.

--- spec/features/cops_spec.rb
+++ spec/features/cops_spec.rb
@@ -1,24 +1,25 @@
 require 'spec_helper'
 
-feature "cops", :type => :feature do 
+feature "cops", :type => :feature do
+  before do
+    @cop = FactoryGirl.create(:cop)
+    visit '/'
+  end
 
   scenario "finds a cop by badge number" do
-    cop = FactoryGirl.create(:cop)
-    visit '/'
-    fill_in 'badge_number', with: cop.badge_number
+    fill_in 'badge_number', with: @cop.badge_number
     click_button 'Search'
 
-    expect(page).to have_text cop.name
-    expect(page).to have_text cop.badge_number
+    expect(page).to have_text @cop.name
+    expect(page).to have_text @cop.badge_number
   end
 
   scenario "finds a cop by name" do
-    cop = FactoryGirl.create(:cop)
-    visit '/'
-    fill_in 'name', with: cop.name
+    fill_in 'name', with: @cop.name
     click_button 'Search'
 
-    expect(page).to have_text cop.name
-    expect(page).to have_text cop.badge_number
+    expect(page).to have_text @cop.name
+    expect(page).to have_text @cop.badge_number
   end
+
 end
