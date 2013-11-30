151f40e9beb54d523db682fd34a1bf54e1aaf0cc
Desmond Rawls <captaingrover@gmail.com>
2013-11-30 00:20:26 -0500

    Passes tests for finding a cop by name. CopsController needs refactoring.

--- app/controllers/cops_controller.rb
+++ app/controllers/cops_controller.rb
@@ -1,5 +1,9 @@
 class CopsController < ApplicationController
   def index
-    @cops = Cop.where(:badge_number => params[:badge_number])
+    if params[:badge_number] && params[:badge_number] != ""
+      @cops = Cop.where(:badge_number => params[:badge_number])
+    elsif params[:name] && params[:name] != ""
+      @cops = Cop.where("name like ?", "%#{params[:name]}%")
+    end
   end
 end
