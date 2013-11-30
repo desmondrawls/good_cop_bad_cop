0108db56cbb22a6711b79560b2ab7a765ee3bb6e
Desmond Rawls <captaingrover@gmail.com>
2013-11-29 23:56:10 -0500

    Builds a failing test for finding a cop by name. Adds to the find-by-badge-number test with an assertion for displaying text with the cops badge number.

--- spec/features/cops_spec.rb
+++ spec/features/cops_spec.rb
@@ -2,12 +2,23 @@ require 'spec_helper'
 
 feature "cops", :type => :feature do 
 
-  scenario "finds a cop" do
+  scenario "finds a cop by badge number" do
     cop = FactoryGirl.create(:cop)
     visit '/'
     fill_in 'badge_number', with: cop.badge_number
     click_button 'Search'
 
     expect(page).to have_text cop.name
+    expect(page).to have_text cop.badge_number
+  end
+
+  scenario "finds a cop by name" do
+    cop = FactoryGirl.create(:cop)
+    visit '/'
+    fill_in 'name', with: cop.name
+    click_button 'Search'
+
+    expect(page).to have_text cop.name
+    expect(page).to have_text cop.badge_number
   end
 end
