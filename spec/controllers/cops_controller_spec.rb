cc72e9bbdab4ebfe310ee0d92c3bcdab6dc0b4cc
Desmond Rawls <captaingrover@gmail.com>
2013-11-30 00:06:20 -0500

    Builds a failing spec for the cops#index action with params[:name].

--- spec/controllers/cops_controller_spec.rb
+++ spec/controllers/cops_controller_spec.rb
@@ -4,10 +4,19 @@ describe CopsController do
 
   describe "#index" do
     context "with params[:badge_number]" do
-      it "should return an array cop with that badge number" do
+      it "should return an array of cops with that badge number - assuming only one" do
         cop = FactoryGirl.create(:cop)
 
         get :index, badge_number: cop.badge_number
+        assigns[:cops].should == [ cop ]
+      end
+    end
+
+    context "with params[:name]" do
+      it "should return an array of cops with that name" do
+        cop = FactoryGirl.create(:cop)
+
+        get :index, name: cop.name
         assigns[:cops].should == [ cop ]
       end
     end
