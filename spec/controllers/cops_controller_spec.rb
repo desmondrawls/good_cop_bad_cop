d0cd3f04e2a90b6dfa12decb41ca76ff632fc4f2
Desmond Rawls <captaingrover@gmail.com>
2013-11-30 13:07:07 -0500

    Refactors cops_controller_spec with a before do.

--- spec/controllers/cops_controller_spec.rb
+++ spec/controllers/cops_controller_spec.rb
@@ -3,28 +3,26 @@ require 'spec_helper'
 describe CopsController do 
 
   describe "#index" do
+    before do 
+      @cop = FactoryGirl.create(:cop)
+    end
+
     context "with params[:badge_number]" do
       it "should return an array of cops with that badge number - assuming only one" do
-        cop = FactoryGirl.create(:cop)
-
-        get :index, badge_number: cop.badge_number
-        assigns[:cops].should == [ cop ]
+        get :index, badge_number: @cop.badge_number
+        assigns[:cops].should == [ @cop ]
       end
     end
 
     context "with params[:name]" do
       it "should return an array of cops with that name" do
-        cop = FactoryGirl.create(:cop)
-
-        get :index, name: cop.name
-        assigns[:cops].should == [ cop ]
+        get :index, name: @cop.name
+        assigns[:cops].should == [ @cop ]
       end
     end
 
     context "without params" do
       it "should not build an array of cops" do
-        cop = FactoryGirl.create(:cop)
-
         get :index
         assigns[:cops].should == nil
       end
